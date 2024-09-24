b37579581d01c2a1be6816c01d2d74a330699a0b
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-09-24 16:26:45 +0100

    chore: deploy fix

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import Login from "../Pages/Login";
-import Register from "../Pages/Register";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 // import HomeLayout from "../layouts/HomeLayout.jsx";
 import LandingPage from "../pages/LandingPage.jsx";
 // import ProfileLayout from "../layouts/ProfileLayout";
